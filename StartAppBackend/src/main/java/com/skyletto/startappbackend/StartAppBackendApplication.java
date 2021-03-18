7da8ebc668cafdf08901bfc59e07f6e21f3af9e1
Mortd3kay <mortd3kay@users.noreply.github.com>
2021-03-18 17:27:23 +0300

    init

---
+++ StartAppBackend/src/main/java/com/skyletto/startappbackend/StartAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.skyletto.startappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartAppBackendApplication.class, args);
+    }
+
+}
---
+++ StartAppBackend/src/test/java/com/skyletto/startappbackend/StartAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skyletto.startappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
