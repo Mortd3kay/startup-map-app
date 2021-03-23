3bc4720924a433e4272f0f694c828af344f6cead
Mortd3kay <mortd3kay@users.noreply.github.com>
2021-03-23 13:16:39 +0300

    refactoring, dataBinding

--- StartAppFrontend/app/src/main/java/com/skyletto/startappfrontend/ui/auth/fragments/LoginFragment.java
+++ StartAppFrontend/app/src/main/java/com/skyletto/startappfrontend/ui/auth/fragments/LoginFragment.java
@@ -4,24 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
+import android.widget.Toast;
 
-import com.google.android.material.button.MaterialButton;
 import com.skyletto.startappfrontend.R;
+import com.skyletto.startappfrontend.databinding.FragmentLoginBinding;
 import com.skyletto.startappfrontend.ui.auth.ActivityStepper;
 import com.skyletto.startappfrontend.ui.auth.viewmodels.LoginAuthViewModel;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 public class LoginFragment extends Fragment {
 
-    private ActivityStepper mActivity;
+    public ActivityStepper mActivity;
     private LoginAuthViewModel viewModel;
-
-    private EditText email;
-    private EditText password;
-    private MaterialButton loginBtn;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -36,34 +33,23 @@ public class LoginFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        viewModel = new ViewModelProvider(getActivity()).get(LoginAuthViewModel.class);
+        viewModel = new ViewModelProvider(this).get(LoginAuthViewModel.class);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_login, container, false);
-        email = (EditText) v.findViewById(R.id.auth_email_input_field);
-        password = (EditText) v.findViewById(R.id.auth_password_input_field);
-        loginBtn = (MaterialButton) v.findViewById(R.id.auth_login_btn);
+        FragmentLoginBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false);
+        View v = binding.getRoot();
 
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                viewModel.setEmail(email.getText().toString());
-                viewModel.setPassword(password.getText().toString());
-                viewModel.login(mActivity);
-            }
+        viewModel.setOnErrorLoginListener(() -> {
+            Toast.makeText(getContext(), "Неверные данные", Toast.LENGTH_SHORT).show();
         });
 
+        viewModel.setOnSuccessLoginListener(() -> {
+            mActivity.onFinish();
+        });
         return v;
     }
 
-    public void nextStep(){
-        mActivity.nextStep();
-    }
-
-    public void prevStep(){
-        mActivity.prevStep();
-    }
 }
--- StartAppFrontend/app/src/main/java/com/skyletto/startappfrontend/ui/auth/viewmodels/LoginAuthViewModel.java
+++ StartAppFrontend/app/src/main/java/com/skyletto/startappfrontend/ui/auth/viewmodels/LoginAuthViewModel.java
@@ -2,11 +2,9 @@ package com.skyletto.startappfrontend.ui.auth.viewmodels;
 
 import android.annotation.SuppressLint;
 import android.app.Application;
-import android.widget.Toast;
 
 import com.skyletto.startappfrontend.data.network.ApiRepository;
 import com.skyletto.startappfrontend.data.requests.LoginDataRequest;
-import com.skyletto.startappfrontend.ui.auth.ActivityStepper;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
@@ -15,29 +13,39 @@ import io.reactivex.schedulers.Schedulers;
 
 public class LoginAuthViewModel extends AndroidViewModel {
     
-    private String email;
-    private String password;
-    
+    private LoginDataRequest data;
+    private OnSuccessLoginListener onSuccessLoginListener;
+    private OnErrorLoginListener onErrorLoginListener;
+
     public LoginAuthViewModel(@NonNull Application application) {
         super(application);
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     @SuppressLint("CheckResult")
-    public void login(ActivityStepper activityStepper) {
-        ApiRepository.getInstance().apiService.login(new LoginDataRequest(email, password))
+    public void login() {
+        ApiRepository.getInstance().apiService.login(data)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
-                        profileResponse -> activityStepper.onFinish(),
-                        throwable -> Toast.makeText(getApplication(), "Неправильно введенные данные", Toast.LENGTH_SHORT).show()
+                        profileResponse -> {if (onSuccessLoginListener!=null) onSuccessLoginListener.onSuccess();},
+                        throwable -> {if (onErrorLoginListener!=null) onErrorLoginListener.onError();}
                 );
     }
+
+    public LoginDataRequest getData() {
+        return data;
+    }
+
+    public void setData(LoginDataRequest data) {
+        this.data = data;
+    }
+
+    public void setOnSuccessLoginListener(OnSuccessLoginListener onSuccessLoginListener) {
+        this.onSuccessLoginListener = onSuccessLoginListener;
+    }
+
+    public void setOnErrorLoginListener(OnErrorLoginListener onErrorLoginListener) {
+        this.onErrorLoginListener = onErrorLoginListener;
+    }
 }
+
