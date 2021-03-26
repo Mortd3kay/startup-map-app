1972d931d7dfbafb0f7ee5b083a410db48b638ba
Mortd3kay <mortd3kay@users.noreply.github.com>
2021-03-26 13:15:35 +0300

    third step in stepper

---
+++ StartAppBackend/src/main/java/com/skyletto/startappbackend/entities/City.java
@@ -0,0 +1,38 @@
+package com.skyletto.startappbackend.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cities")
+public class City {
+    @Id
+    @GeneratedValue()
+    private int id;
+    private String name;
+
+    public City(String name) {
+        this.name = name;
+    }
+
+    public City() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
