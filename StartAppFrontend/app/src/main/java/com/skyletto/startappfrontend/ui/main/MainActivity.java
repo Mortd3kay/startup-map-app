9d9aab7a8509e4a5e4f6b2a1872ee5ca38ca80fc
Mortd3kay <mortd3kay@users.noreply.github.com>
2021-04-04 15:09:31 +0300

    map implementation

--- StartAppFrontend/app/src/main/java/com/skyletto/startappfrontend/ui/main/MainActivity.java
+++ StartAppFrontend/app/src/main/java/com/skyletto/startappfrontend/ui/main/MainActivity.java
@@ -1,22 +1,23 @@
 package com.skyletto.startappfrontend.ui.main;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import com.skyletto.startappfrontend.R;
+import com.skyletto.startappfrontend.ui.main.fragments.MapsFragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextView;
+    private FragmentManager fm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mTextView = (TextView) findViewById(R.id.text);
+        fm = getSupportFragmentManager();
+        fm.beginTransaction().replace(R.id.main_pane,fm.getFragmentFactory().instantiate(ClassLoader.getSystemClassLoader(), MapsFragment.class.getName())).commitNow();
 
     }
 }
---
+++ StartAppFrontend/app/src/main/java/com/skyletto/startappfrontend/ui/main/fragments/MapsFragment.java
@@ -0,0 +1,73 @@
+package com.skyletto.startappfrontend.ui.main.fragments;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.skyletto.startappfrontend.R;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MapsFragment extends Fragment {
+
+    private static final String TAG = "MAP_FRAGMENT";
+    GoogleMap mMap;
+
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+
+            mMap = googleMap;
+            try {
+                if (!mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(requireContext(), R.raw.mapstyle)))
+                    Log.e(TAG, "onMapReady: Parsing failed");
+
+            } catch (Exception e){
+                Log.e(TAG, "onMapReady: ", e.getCause());
+            }
+
+            LatLng sydney = new LatLng(-34, 151);
+            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        }
+    };
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_maps, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
+    }
+}
