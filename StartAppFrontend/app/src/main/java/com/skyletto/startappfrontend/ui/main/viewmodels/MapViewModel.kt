35a4017179a0bfa0699ba8a2f2e25b072090d65e
Mortd3kay <mortd3kay@users.noreply.github.com>
2021-05-03 15:59:09 +0300

    toolbar demo

--- StartAppFrontend/app/src/main/java/com/skyletto/startappfrontend/ui/main/viewmodels/MapViewModel.kt
+++ StartAppFrontend/app/src/main/java/com/skyletto/startappfrontend/ui/main/viewmodels/MapViewModel.kt
@@ -3,4 +3,6 @@ package com.skyletto.startappfrontend.ui.main.viewmodels
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 
-class MapViewModel(application: Application) : AndroidViewModel(application)
+class MapViewModel(application: Application) : AndroidViewModel(application){
+    val categories = arrayListOf("All", "Specs", "Projects")
+}
