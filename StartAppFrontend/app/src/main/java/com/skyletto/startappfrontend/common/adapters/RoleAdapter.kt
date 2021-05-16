fba351fb25a5bd3685c410caa873c9115b793eeb
Mortd3kay <mortd3kay@users.noreply.github.com>
2021-05-16 12:25:16 +0300

    deleting roles works

--- StartAppFrontend/app/src/main/java/com/skyletto/startappfrontend/common/adapters/RoleAdapter.kt
+++ StartAppFrontend/app/src/main/java/com/skyletto/startappfrontend/common/adapters/RoleAdapter.kt
@@ -18,12 +18,12 @@ import com.skyletto.startappfrontend.domain.entities.ProjectRole
 class RoleAdapter(private val context: Context, private val roleTypes: List<ProjectRole>) : RecyclerView.Adapter<RoleAdapter.RoleViewHolder>() {
 
     var roles = mutableListOf<ProjectRoleItem>()
-        set(value){
+        set(value) {
             field = value
             notifyDataSetChanged()
         }
 
-    fun addRole(project:Project){
+    fun addRole(project: Project) {
         val role = ProjectRoleItem()
         role.project = project
         roles.add(role)
@@ -33,8 +33,30 @@ class RoleAdapter(private val context: Context, private val roleTypes: List<Proj
 
     inner class RoleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val binding: RoleItemBinding? = DataBindingUtil.bind(itemView)
+
         init {
-            binding?.roleSpinner?.adapter = ArrayAdapter(itemView.context, R.layout.support_simple_spinner_dropdown_item, roleTypes.map { it.name })
+            binding?.let {
+                it.roleSpinner.adapter = ArrayAdapter(itemView.context, R.layout.support_simple_spinner_dropdown_item, roleTypes.map { innerIt -> innerIt.name })
+                it.click = View.OnClickListener {
+                    roles.removeAt(adapterPosition)
+                    notifyItemRemoved(adapterPosition)
+                }
+                it.roleSalaryType.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(parent: AdapterView<*>?, view: View?, _position: Int, id: Long) {
+                        val char = (it.roleSalaryType.selectedItem as String)[0]
+                        it.item?.salaryType?.set(char)
+                        it.roleSalaryAmount.let { innerIt ->
+                            if (char == '%' && innerIt.text.length > 2) {
+                                innerIt.setText(innerIt.text.substring(0, 2))
+                            }
+                        }
+                    }
+
+                    override fun onNothingSelected(parent: AdapterView<*>?) {
+                        TODO("Not yet implemented")
+                    }
+                }
+            }
         }
     }
 
@@ -45,29 +67,11 @@ class RoleAdapter(private val context: Context, private val roleTypes: List<Proj
 
     override fun onBindViewHolder(holder: RoleViewHolder, position: Int) {
         holder.binding?.item = roles[position]
-        holder.binding?.roleSalaryType?.let{
-            it.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(parent: AdapterView<*>?, view: View?, _position: Int, id: Long) {
-                    roles[position].salaryType.set((it.selectedItem as String)[0])
-                    holder.binding.roleSalaryAmount.let { innerIt->
-                        if ((it.selectedItem as String)[0] == '%' && innerIt.text.length > 2){
-                            innerIt.setText(innerIt.text.substring(0,2))
-                        }
-                    }
-
-                }
-
-                override fun onNothingSelected(parent: AdapterView<*>?) {
-                    TODO("Not yet implemented")
-                }
-
-            }
-        }
     }
 
     override fun getItemCount() = roles.size
 
-    companion object{
+    companion object {
         const val TAG = "ROLE_ADAPTER"
     }
 }
