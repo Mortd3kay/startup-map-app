577023110d120225dea3d11824a36c048a7962df
Mortd3kay <mortd3kay@users.noreply.github.com>
2021-05-25 14:08:17 +0300

    alert dialog fine

--- StartAppFrontend/app/src/main/java/com/skyletto/startappfrontend/ui/main/fragments/MapDialog.kt
+++ StartAppFrontend/app/src/main/java/com/skyletto/startappfrontend/ui/main/fragments/MapDialog.kt
@@ -6,11 +6,14 @@ import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import androidx.core.content.res.ResourcesCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 import com.skyletto.startappfrontend.R
 import com.skyletto.startappfrontend.common.MainApplication
 import com.skyletto.startappfrontend.common.models.AlertModel
@@ -18,25 +21,35 @@ import com.skyletto.startappfrontend.common.models.ProjectWithTagsAndRoles
 import com.skyletto.startappfrontend.common.models.UserWithTags
 import com.skyletto.startappfrontend.data.database.AppDatabase
 import com.skyletto.startappfrontend.databinding.MapAlertDialogBinding
+import com.skyletto.startappfrontend.domain.entities.Tag
 import com.skyletto.startappfrontend.ui.chat.ChatActivity
 
-class MapDialog(val model : AlertModel) : DialogFragment() {
+class MapDialog(val model: AlertModel) : DialogFragment() {
     private val mModel = MutableLiveData(model)
     private lateinit var db: AppDatabase
-    private var binding : MapAlertDialogBinding?=  null
+    private var binding: MapAlertDialogBinding? = null
     private lateinit var project: LiveData<ProjectWithTagsAndRoles>
     private lateinit var user: LiveData<UserWithTags>
 
     init {
         mModel.observeForever {
             binding?.model = it
-            binding?.click = View.OnClickListener { iit->
-                if (it.chatId!=null){
+            binding?.click = View.OnClickListener { _ ->
+                if (it.chatId != null) {
                     val intent = Intent(context, ChatActivity::class.java)
                     intent.putExtra("id", it.chatId)
                     context?.startActivity(intent)
                 }
-
+            }
+            binding?.mapDialogImg?.setImageResource(
+                    if (it.isProject) {
+                        R.drawable.ic_idea_image
+                    } else {
+                        R.drawable.ic_profile_image
+                    }
+            )
+            binding?.mapDialogChipGroup?.let { iit->
+                it.tags?.let { it2-> inflateChipGroup(iit, it2) }
             }
         }
     }
@@ -45,7 +58,7 @@ class MapDialog(val model : AlertModel) : DialogFragment() {
         super.onCreate(savedInstanceState)
         val app = activity?.application as MainApplication
         db = app.db
-        if (model.isProject){
+        if (model.isProject) {
             project = db.projectDao().getById(model.id)
             observeProject()
         } else {
@@ -55,33 +68,33 @@ class MapDialog(val model : AlertModel) : DialogFragment() {
     }
 
     private fun observeUser() {
-        user.observe(this){ oit->
+        user.observe(this) { oit ->
             mModel.value?.let {
-                it.title = oit.user.title?:""
+                it.title = oit.user.title ?: ""
                 it.tags = oit.tags
-                it.subtitle = oit.user.firstName +" "+ oit.user.secondName
-                it.subsubtitle = getString(R.string.exp_of_work)+oit.user.experience
-                it.description = oit.user.description?:""
+                it.subtitle = oit.user.firstName + " " + oit.user.secondName
+                it.subsubtitle = getString(R.string.exp_of_work) + oit.user.experience ?: "Нет"
+                it.description = oit.user.description ?: ""
                 it.chatId = oit.user.id
             }
             mModel.postValue(mModel.value)
         }
     }
 
-    private fun observeProject(){
-        project.observe(this){ oit->
+    private fun observeProject() {
+        project.observe(this) { oit ->
             mModel.value?.let {
                 it.title = oit.project.title
                 it.subtitle = ""
                 val str = getString(R.string.needed) + oit.roles?.joinToString { iit ->
-                    if (iit.user==null){
+                    if (iit.user == null) {
                         return@joinToString iit.role?.name!!
                     }
                     return@joinToString ""
-                }
+                }?.replace(" ,", "")?.trim(',')?.toLowerCase()
                 Log.d(TAG, "onCreate: subtitle $str")
                 if (str != null) {
-                    it.subsubtitle =str
+                    it.subsubtitle = str
                 }
                 it.description = oit.project.description
                 it.tags = oit.tags
@@ -107,12 +120,22 @@ class MapDialog(val model : AlertModel) : DialogFragment() {
     override fun onResume() {
         super.onResume()
         dialog?.window?.setLayout(
-                resources.displayMetrics.widthPixels*85/100,
-                resources.displayMetrics.heightPixels*50/100
+                resources.displayMetrics.widthPixels * 85 / 100,
+                resources.displayMetrics.heightPixels * 50 / 100
         )
     }
 
-    companion object{
+    private fun inflateChipGroup(group: ChipGroup, tags: Set<Tag>) {
+        group.removeAllViews()
+        for (t in tags) {
+            val chip = (Chip(requireContext()))
+            chip.text = t.name
+            chip.chipBackgroundColor = ResourcesCompat.getColorStateList(resources, R.color.skin2, null)
+            group.addView(chip)
+        }
+    }
+
+    companion object {
         private const val TAG = "MAP_DIALOG"
     }
 }
